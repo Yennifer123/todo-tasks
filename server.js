bddcd87032b9f427f32e17c6849af9d7308fa877
yennifer123 <jemp1886@hotmail.com>
2023-04-15 07:42:11 -0500

    add api task shema .env

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const api = require('./api');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const app = express();
 
-
+app.use(bodyParser.json());
 app.listen(port, function () {
     console.log("Server is listening at port: " + port);
 });
@@ -11,3 +13,4 @@ app.get('/', function (req, res) {
     res.send("hello world");
 });
 
+app.use('/api', api);
